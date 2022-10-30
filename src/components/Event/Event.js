a5444f534df3679f08d06f2c16a8786dcc65c541
Emran12 <emran6452@gmail.com>
2022-10-30 11:24:29 +0600

    event added

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -7,21 +7,28 @@ import EventInfo from "../EventInfo/EventInfo";
 const Event = () => {
   return (
     <div className="eventContainer">
-      <h1 className="eventHeader">Upcoming Events</h1>
-      <p className="eventInfo">
+      <h1 className="eventTitle">Upcoming Events</h1>
+      <p className="eventSubTitle">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum
         augue quis augue ornare, eget faucibus felis pharetra. Lorem ipsum dolor
         sit amet, consectetur adipiscing elit.
       </p>
-      <div>
+      <div className="event">
         <div>
-          <img src={event1} />
+          <img src={event1} width="590px" height="551px" />
         </div>
         <div>
           <EventInfo></EventInfo>
         </div>
       </div>
-      <div></div>
+      <div className="event" id="event2">
+        <div>
+          <EventInfo></EventInfo>
+        </div>
+        <div>
+          <img src={event2} width="590px" height="551px" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/EventInfo/EventInfo.js
+++ src/components/EventInfo/EventInfo.js
@@ -6,9 +6,23 @@ import calendar from "../../assets/icons/Calendar.png";
 
 const EventInfo = () => {
   return (
-    <div>
-      <h1>How to lead a good healthy life</h1>
-      <p>
+    <div className="eventInfoContainer">
+      <h1 className="eventHeader">How to lead a good healthy life</h1>
+      <div className="eventSubContainer">
+        <p className="eventSubInfo">
+          <img src={calendar} alt="" />
+          <span>15 Oct, 2019</span>
+        </p>
+        <p className="eventSubInfo">
+          <img src={clock} alt="" />
+          <span>8:00 AM - 5:00 PM</span>
+        </p>
+        <p className="eventSubInfo">
+          <img src={location} alt="" />
+          <span>Uttara, TX 70240</span>
+        </p>
+      </div>
+      <p className="eventInfo">
         Early had add equal china quiet visit. Appear an manner as no limits
         either praise in. In in written on charmed justice is amiable farther
         besides. Law insensible middletons unsatiable for apartments boy
@@ -17,12 +31,6 @@ const EventInfo = () => {
         is amiable farther besides. Law insensible middletons unsatiable for
         apartments boy delightful unreserved.
       </p>
-      <div>
-        <p>
-          <img src={calendar} alt="" />
-          <span></span>
-        </p>
-      </div>
     </div>
   );
 };
